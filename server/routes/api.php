eb2b37bf8cb8f8e23066de0a70f5d7f51b087e74
ChaClyde <carca@filamer.edu.ph>
2026-04-11 10:55:48 +0800

    Added Edit and Delete

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\Api\GenderController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route; 
 
 Route::controller(GenderController::class)->prefix('/gender')->group(function () {
-    Route::get('loadGenders', 'loadGenders'); 
-    Route::post('/storeGender', 'storeGender');
+    Route::get('/loadGenders', 'loadGenders');  //gender/loadGenders
+    Route::get('/getGender/{genderId}', 'getGender');
+    Route::post('/storeGender', 'storeGender');  //gender/storeGender
+    Route::put('/updateGender/{gender}', 'updateGender');
+    Route::put('/destroyGender/{gender}', 'destroyGender');
 });
 
 // Route::get('/user', function (Request $request) {
